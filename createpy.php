3303fec22af639e0242be7cc3dcc9a0d1c84cbe9
Andychoi153 <Andychoi153@gmail.com>
2016-02-15 14:35:26 +0900

    Update createpy.php

--- createpy.php
+++ createpy.php
@@ -17,8 +17,7 @@
       </div>
     <div data-role="content">
 <?php
-// 4.1.0 이전의 PHP에서는, $_FILES 대신에 $HTTP_POST_FILES를
-// 사용해야 합니다.z
+
 session_start();
 $user_id = $_SESSION['user_id'];
 $user_name = $_SESSION['user_name'];
@@ -26,8 +25,8 @@ $store = 0;
 $category = $_POST["category"];
 $name =  $_POST["name"];
 
-$connect = mysql_connect("localhost", "root", "eqrqrr9326") or die("cannot connect to mysql"); //username과 pw_user는 mysql 사용자, 암호이며 해당시스템에 맞게 수정
-mysql_select_db("mysql", $connect); //mysql에는 기본으로 mysql과 test DB가 있음
+$connect = mysql_connect("localhost", "root", "eqrqrr9326") or die("cannot connect to mysql"); 
+mysql_select_db("mysql", $connect); 
 $quel = mysql_query("SELECT * FROM FILTER WHERE CREATOR ='".$user_id."' AND FILTER_NAME = '".$name.".py'");
 $count = mysql_num_rows($quel);
 
