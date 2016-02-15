a18d287b8c38063afdda4b5b841e05b6ad4f1955
Andychoi153 <Andychoi153@gmail.com>
2016-02-15 14:18:49 +0900

    upload python file session

---
+++ mbupif.php
@@ -0,0 +1,36 @@
+<head>
+<style type="text/css">
+
+.button {
+  display: inline-block;
+  padding: .2em .35em;
+  color: #999;
+  font-size: inherit;
+  line-height: normal;
+  vertical-align: middle;
+  background-color: #fdfdfd;
+  cursor: pointer;
+  border: 1px solid #ebebeb;
+  border-bottom-color: #e2e2e2;
+  border-radius: .25em;
+}
+
+</style>
+
+</head>
+
+<form method="POST" ENCTYPE="multipart/form-data" action="up_filter.php">
+            
+		
+	    <tr>
+                    <td width="600" colspan="2" align="center"><b>Fitting function upload</b></td>
+            </tr>
+            <tr>
+                    <td width="150" align="center">Only python</td>
+
+                    <td width="450"> <input type="file" name="userfile" size="20" class="button"><br/>(File limit : 8MB)</td>
+            </tr>
+	<input type="submit" value="send" class="button"> 
+		
+	   
+</form>
